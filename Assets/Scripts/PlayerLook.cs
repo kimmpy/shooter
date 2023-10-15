cb0bf505f3b39ef0b035116e9b6e8538d6290817
kimmpy <kim.kp.pham@gmail.com>
2023-10-14 23:04:20 -0700

    movement/looking

---
+++ Assets/Scripts/PlayerLook.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+    public Camera cam;
+    private float xRotation = 0f;
+    
+    public float xSens = 30f;
+    public float ySens = 30f;
+    // Start is called before the first frame update
+    public void ProcessLook(Vector2 input)
+    {
+        float mouseX = input.x;
+        float mouseY = input.y;
+
+        //calculate cam rotation for looking up/down
+        xRotation -= (mouseY * Time.deltaTime) * ySens;
+        xRotation = Mathf.Clamp(xRotation, -80f, 80f);
+        cam.transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        //rotate player to look left/right
+        transform.Rotate(Vector3.up * (mouseX * Time.deltaTime) * xSens);
+    }
+}
